93cbe90e628b983b30f5f80ec6d42b37b05a4684
Matt <mattdhiscocks@gmail.com>
2022-01-30 22:05:54 +0000

    day 24 complete

--- day_24/solution.py
+++ day_24/solution.py
@@ -1,16 +1,27 @@
+from math import floor
 
 bad_states   = set()
 z_div_lookup = [1 , 1 , 1 , 1 , 1 , 26,  1,  26,  26,  1,  26, 26, 26, 26]
-x_add_lookup = [10, 12, 13, 11, 14, -2, 11, -15, -10, 10, -10, -4, -1, -1]
-y_add_lookup = [0 , 1 , 1 , 4 ,  9,  1, 10,   6,   4,  6,   3,  9, 15,  5]
+x_add_lookup = [10, 12, 13, 13, 14, -2, 11, -15, -10, 10, -10, -4, -1, -1]
+y_add_lookup = [0 , 6 , 4 , 2 ,  9,  1, 10,   6,   4,  6,   3,  9, 15,  5]
+
+
+hits = 0
+misses = 0
 
 def solve(digits, depth, prev_z, num):
+    global hits
+    global misses
 
     if (depth, prev_z) in bad_states or depth==14:
+        hits += 1
         return None
+    
+    misses += 1
 
-    if depth == 0:
-        print(num)
+    if depth == 3:
+        print(num, hits, round(hits/ (misses+hits), 2))
+        hits = misses = 0
 
     # Shift along one base 10
     num *= 10
@@ -21,7 +32,7 @@ def solve(digits, depth, prev_z, num):
         w = i
         x = z
         x %= 26
-        z /= z_div_lookup[depth]
+        z = int(z / z_div_lookup[depth])
         x += x_add_lookup[depth]
         x = int(x == w)
         x = int(x == 0)
@@ -45,11 +56,11 @@ def solve(digits, depth, prev_z, num):
 
 
 def part1():
-    digits = range(9, -1, -1)
+    digits = range(9, 0, -1)
     print(solve(digits, 0, 0, 0))
 
 def part2():
-    digits = range(0, 10, 1)
+    digits = range(1, 10, 1)
     print(solve(digits, 0, 0, 0))
 
 
